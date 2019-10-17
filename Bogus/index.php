b06c41150557d8861ff39275705ea03648b8991c
Corbin <cvida526@uwsp.edu>
2019-10-17 14:12:11 -0500

    Removing Bogus stuff

--- Bogus/index.php
+++
@@ -1,1 +0,0 @@
-<?php header("Location: views/home.php"); ?>
--- Bogus/views/footer.php
+++
@@ -1,7 +0,0 @@
-				<!-- ##### Footer ##### -->
-				<footer>
-					<p>&copy; 2019 Tim Bubla, All Rights Reserved</p>
-				</footer>
-			<!--/div>
-		</body>
-</html-->
