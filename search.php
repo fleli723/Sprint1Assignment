c323c93633c8cde3cdff5177aec2aafe8782dce0
Lelis <fleli723@uwsp.edu>
2019-10-16 19:05:07 -0500

    refactoring

--- search.php
+++ search.php
@@ -3,30 +3,37 @@
 require_once("Template.php");
 
 $page = new Template("Search Page");
-$page->addHeadElement("<script src='hello.js'></script>");
-$page->addHeadElement("<link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheet.css\">");
+$page->addHeadElement('<script src="hello.js"></script>');
+$page->addHeadElement('<link rel="stylesheet" type="text/css" href="stylesheet.css">');
 $page->finalizeTopSection();
 $page->finalizeBottomSection();
 
 print $page->getTopSection();
 
-print 	'<div class="topbar">
-			<h1> CNMT 310 Sprint 1 Assignment</h1>
-			<ul class="nav">
-				<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/index.php">Home</a></li>
-				<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/survey.php">Survey</a></li>
-				<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/privacy.php">Privacy Policy</a></li>
-				<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/search.php">Search</a></li>
-			</ul>
-		</div>';
+print	'
+<div class="topbar">
+	<h1> CNMT 310 Sprint 1 Assignment</h1>
+	<ul class="nav">
+		<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/index.php">Home</a></li>
+		<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/survey.php">Survey</a></li>
+		<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/privacy.php">Privacy Policy</a></li>
+		<li><a href="http://cnmtsrv2.uwsp.edu/~fleli723/Sprint1Assignment/search.php">Search</a></li>
+	</ul>
+</div>
+';
 
-print	'<form name="myForm" method="post" action="testform.php" onsubmit="return validateForm()">
-
-			<span>Search</span>
-			<input type="text" id="SearchBar" name="Search_Bar_Name" placeholder="Title or Artist Name">
-			<br>
-			<input type="submit" value="Submit" id="Btnsubmit">
-		</form>';
+print	'
+<div class="content">
+	<form name="myForm" onsubmit="return validateForm();" action="testform.php" method="post">
+		
+		<span>Search: </span>
+		<input type="text" id="SearchBar" name="Search_Bar_Name" placeholder="Title or Artist Name">
+		<br>
+		<input type="submit" value="Submit" id="Btnsubmit">
+		
+	</form>
+</div>	
+';
 
 print $page->getBottomSection();
 
--- survey.php
+++ survey.php
@@ -20,51 +20,49 @@ print 	'<div class="topbar">
 			</ul>
 		</div>';
 			
-print		"<div class=\"content\">
-			
-			<form name=\"survey\" onsubmit=\"return validateForm();\" action=\"action.php\" method=\"post\">
-			
-			Email:<br>
-			<input type=\"text\" id=\"email\" name=\"email\"><br>
-			<br>
-			
-			<div class=\"formboxes\">
-			
-				What is your major?<br>
-				<input type=\"checkbox\" name=\"major\" value=\"CIS-AppDev\"> CIS-AppDev<br>
-				<input type=\"checkbox\" name=\"major\" value=\"CIS-Networking\"> CIS-Networking<br>
-				<input type=\"checkbox\" name=\"major\" value=\"WDMD\"> WDMD<br>
-				<input type=\"checkbox\" name=\"major\" value=\"WD\"> WD<br>
-				<input type=\"checkbox\" name=\"major\" value=\"HTI\"> HTI<br>
-				<input type=\"checkbox\" name=\"major\" value=\"Other\"> Other<br>
-				<br>
-			</div>
-			
-			<div class=\"formboxes\">
-				What grade do you expect to receive in CNMT 310?<br>
-				<input type=\"radio\" name=\"grade\" value=\"A\"> A<br>
-				<input type=\"radio\" name=\"grade\" value=\"B\"> B<br>
-				<input type=\"radio\" name=\"grade\" value=\"C\"> C<br>
-				<input type=\"radio\" name=\"grade\" value=\"D\"> D<br>
-				<input type=\"radio\" name=\"grade\" value=\"F\"> F<br>
-				<br>
-			</div>
-			
-			<div class=\"formboxes\">
-				What is your favorite pizza topping?<br>
-				<input type=\"radio\" name=\"topping\" value=\"Pepperoni\"> Pepperoni<br>
-				<input type=\"radio\" name=\"topping\" value=\"Sausage\"> Sausage<br>
-				<input type=\"radio\" name=\"topping\" value=\"Bacon\"> Bacon<br>
-				<input type=\"radio\" name=\"topping\" value=\"Mushroom\"> Mushroom<br>
-				<input type=\"radio\" name=\"topping\" value=\"Pineapple\"> Pineapple<br>
-				<br>
-			</div>
-			
-			<input class=\"button\" type=\"submit\" value=\"Submit\">
-			
-			</form>
+print	'
+<div class="content">		
+	<form name="survey" onsubmit="return validateForm();" action="action.php" method="post">
+		Email:<br>
+		<input type="text" id="email" name="email"><br>
+		<br>
 		
-			</div>";
+		<div class="formboxes">
+			What is your major?<br>
+			<input type="checkbox" name="major" value="CIS-AppDev"> CIS-AppDev<br>
+			<input type="checkbox" name="major" value="CIS-Networking"> CIS-Networking<br>
+			<input type="checkbox" name="major" value="WDMD"> WDMD<br>
+			<input type="checkbox" name="major" value="WD"> WD<br>
+			<input type="checkbox" name="major" value="HTI"> HTI<br>
+			<input type="checkbox" name="major" value="Other"> Other<br>
+			<br>
+		</div>
+			
+		<div class="formboxes">
+			What grade do you expect to receive in CNMT 310?<br>
+			<input type="radio" name="grade" value="A"> A<br>
+			<input type="radio" name="grade" value="B"> B<br>
+			<input type="radio" name="grade" value="C"> C<br>
+			<input type="radio" name="grade" value="D"> D<br>
+			<input type="radio" name="grade" value="F"> F<br>
+			<br>
+		</div>
+			
+		<div class="formboxes">
+			What is your favorite pizza topping?<br>
+			<input type="radio" name="topping" value="Pepperoni"> Pepperoni<br>
+			<input type="radio" name="topping" value="Sausage"> Sausage<br>
+			<input type="radio" name="topping" value="Bacon"> Bacon<br>
+			<input type="radio" name="topping" value="Mushroom"> Mushroom<br>
+			<input type="radio" name="topping" value="Pineapple"> Pineapple<br>
+			<br>
+		</div>
+			
+		<input class="button" type="submit" value="Submit">
+			
+	</form>
+		
+</div>';
 
 print $page->getBottomSection();
 
