04269b361a975f806474258ff0ba36929a89251f
Lelis <fleli723@uwsp.edu>
2019-10-21 19:18:34 -0500

    removed comment

--- php/const.php
+++ php/const.php
@@ -1,14 +1,5 @@
 <?php
 
-//define("MUSER","vidaille_c_admin");
-//define("MPASS","jon52vor");
-//define("MSERVER","cnmtsrv1.uwsp.edu");
-//define("MDB","vidaille_c");
-
-//if (basename($_SERVER['PHP_SELF']) == "const.php") {
-  //die(header("HTTP/1.0 404 Not Found"));
-//}
-
 define("MUSER","bubla_t_admin");
 define("MPASS","xew56baz");
 define("MSERVER","cnmtsrv1.uwsp.edu");
